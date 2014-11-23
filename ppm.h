a5664d73695ca70697d30937772227a81bf789be
Jaure Nathalie <njaure@bs406-s16-11.insa-lyon.fr>
2014-11-23 20:22:45 +0100

    Definition of the class picture and declaration of functions

--- ppm.h
+++ ppm.h
@@ -1,31 +1,60 @@
 
-
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-
-
 //============================================================================
-//                           Function declarations
+//                           Class definition
 //============================================================================
-// Write the image contained in <data> (of size <width> * <height>)
-// into plain RGB ppm file <file>
+
+
+class picture
+{
+
+private:
+
+
+  int width;
+  int height;
+  u_char *image;
+  int x;
+  int y;	
+
+public:
+
+// Definition of Getters
+int getwidth() const;
+int getheight() const;
+u_char* getimage() const;
+
+// Creation of Default constructor
+picture();
+
+// Creation of Copy constructor
+picture(const picture& model);
+
+
+// Definition of Destructor
+~picture();
 
 
 
-
-void ppm_write_to_file(int width, int height, u_char* data, FILE* file, char* img);
+void ppm_write_to_file(FILE* file,const char* img);
 
 // Read the image contained in plain RGB ppm file <file>
 // into <data> and set <width> and <height> accordingly
 // Warning: data is malloc_ed, don't forget to free it
-void ppm_read_from_file(int *width, int *height, u_char** data, FILE* file,char* img );
+void ppm_read_from_file(FILE* file, const char* img );
 
 // Desaturate (transform to B&W) <image> (of size <width> * <height>)
-void ppm_desaturate(u_char* image,int width, int height, int x, int y );
+void ppm_desaturate();
 
 // Shrink image (of original size <width> * <height>) by factor <factor>
 // <width> and <height> are updated accordingly
-void ppm_shrink(u_char** image, int *width, int *height, int factor, int x, int y );
+void ppm_shrink( int factor);
+
+};
+
+
+
