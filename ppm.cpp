01190b232914999247bbd233ff4150c1d27cd5ef
Jaure Nathalie <njaure@bs406-s16-11.insa-lyon.fr>
2014-11-23 20:05:43 +0100

    Modification of the main by using the class picture and the new definitions of functions

--- ppm.cpp
+++ ppm.cpp
@@ -1,132 +1,184 @@
-#include "ppm.h"
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "ppm.h"
+
 //============================================================================
-//                           Structure declarations
+//                           Getters definitions
 //============================================================================
-typedef  struct 
+
+int picture::getwidth() const
 {
-int x;
-int y;
-}Coordonnees;
-
-typedef  struct 
-{
-int width;
-int height;
-u_char *image;
-}Taille;
-
-
-
-//============================================================================
-//                                  Main
-//============================================================================
-int main(int argc, char* argv[])
-{
-  Coordonnees point;
-  Taille size;
-
-  //--------------------------------------------------------------------------
-  // Read file "gargouille.ppm" into image (width and height)
-  //--------------------------------------------------------------------------
-   
-  size.image = NULL;
-  
- FILE* ppm_input=NULL;
- char gar[30]="gargouille.ppm";
-  ppm_read_from_file(&size.width, &size.height, &size.image, ppm_input, gar);
-  
-
-  //--------------------------------------------------------------------------
-  // Create a desaturated (B&W) copy of the image we've just read and
-  // write it into "gargouille_BW.ppm"
-  //--------------------------------------------------------------------------
-  // Copy image into image_bw
-
-  Taille size_bw; //struc pour image_bw
-
-  size_bw.width  = size.width;
-  size_bw.height = size.height;
-  size_bw.image = new u_char[3*size.width * size.height];
-  memcpy(size_bw.image, size.image, 3 * size.width * size.height * sizeof(*size_bw.image));
-
-  // Desaturate image_bw
-  ppm_desaturate(size_bw.image, size.width, size.height, point.x, point.y);
-
-  // Write the desaturated image into "gargouille_BW.ppm"
-  FILE* ppm_output=NULL;
-char gar_bw[30]="gargouille_BW.ppm"; 
-  ppm_write_to_file(size.width, size.height, size_bw.image, ppm_output,gar_bw);
- 
-
-  // Free the desaturated image
-  delete size_bw.image;
-
-
-  //--------------------------------------------------------------------------
-  // Create a resized copy of the image and
-  // write it into "gargouille_small.ppm"
-  //--------------------------------------------------------------------------
-  // Copy image into image_small
-
-  Taille size_small; //struc pour image_small
-
-  size_small.width  = size.width;
-  size_small.height = size.height;
-   size_small.image = new u_char[3*size.width * size.height];
-  memcpy(size_small.image, size.image, 3 * size_small.width * size_small.height * sizeof(*size_small.image));
-
-  // Shrink image_small size 2-fold
-  ppm_shrink(&size_small.image, &size_small.width, &size_small.height, 2, point.x, point.y);
-
-  // Write the desaturated image into "gargouille_small.ppm"
-   FILE* ppm_output2=NULL;
-   char gar_small[40]="gargouille_small.ppm";
-  ppm_write_to_file(size_small.width, size_small.height, size_small.image, ppm_output2, gar_small);
-    
-
-  // Free the not yet freed images
-  delete size.image;
-  delete size_small.image;
-
-  return 0;
+return width;
 }
 
+int picture::getheight() const
+{
+return height;
+}
+
+u_char* picture::getimage() const
+{
+return image;
+}
+
+//============================================================================
+//                           Constructor definition
+//============================================================================
 
 
+picture::picture()
+{
+width = 0;
+height = 0;
+image = new u_char [1];
+}
+
+//============================================================================
+//                           Copy Constructor definition
+//============================================================================
 
 
+picture::picture(const picture& model)
+{
+width = model.getwidth();
+height = model.getheight();
+image = new u_char [3 * width * height];
+memcpy(image, model.getimage(), 3 * width * height * sizeof(*image));
+}
+picture::~picture()
+{
+delete [] image;
+}
+
+//============================================================================
+//                           Function definitions
+//============================================================================
 
 
+void picture::ppm_write_to_file(FILE* file, const char* img)
+{
+
+  file=fopen(img, "wb");
+  // Write header
+  fprintf(file, "P6\n%d %d\n255\n", width, height);
+
+  // Write pixels
+  fwrite(image, 3, width*height, file);
+  fclose(file);
+ 
+}
+
+void picture::ppm_read_from_file(FILE* file, const char* img)
+{
+  file = fopen(img, "rb");
+  
+  
+  // Read file header
+  fscanf(file, "P6\n%d %d\n255\n", &width, &height);
+
+  // Allocate memory according to size.width and height
+  delete [] image;
+  image = new u_char [3 * (width) * (height)];
+
+  // Read the actual image data
+  fread(image, 3, (width) * (height), file);
+  fclose(file);
+
+}
+
+void picture::ppm_desaturate()
+{
+  // For each pixel ...
+  for (x = 0 ; x < width ; x++)
+  {
+    for (y = 0 ; y < height ; y++)
+    {
+      u_int grey_lvl = 0;
+      int rgb_canal;
+
+      // Compute the grey level
+      for (rgb_canal = 0 ; rgb_canal < 3 ; rgb_canal++)
+      {
+        grey_lvl += image[ 3 * (y * width + x) + rgb_canal ];
+      }
+      grey_lvl /= 3;
+      assert(grey_lvl >= 0 && grey_lvl <=255);
+
+      // Set the corresponding pixel's value in new_image
+      memset(&image[3 * (y * width + x)], grey_lvl, 3);
+    }
+  }
+}
+
+void picture::ppm_shrink(int factor)
+{
+ 
+  // Compute new image size and allocate memory for the new image
+  int new_width   = (width) / factor;
+  int new_height  = (height) / factor;
+  u_char* new_image = new u_char [3 * new_width * new_height ];
 
 
+  // Precompute factor^2 (for performance reasons)
+  int factor_squared = factor * factor;
 
+  // For each pixel of the new image...
+ 
+  for (x = 0 ; x < new_width ; x++)
+  {
+    for (y = 0 ; y < new_height ; y++)
+    {
+      // ... compute the average RGB values of the set of pixels (a square of side factor)
+      // that correspond to the pixel we are creating.
 
+      // Initialize RGB values for the new image's pixel
+      u_int red   = 0;
+      u_int green = 0;
+      u_int blue  = 0;
 
+      // Compute coordinates and index of the first (top-left) pixel from the
+      // model image corresponding to the pixel we are creating
+      int x0 = x * factor;
+      int y0 = y * factor;
+      int i0 = 3 * (y0 * (width) + x0);
 
+      // Compute RGB values for the new pixel
+      int dx, dy;
+      for (dx = 0 ; dx < factor ; dx++)
+      {
+        for (dy = 0 ; dy < factor ; dy++)
+        {
+          // Compute the offset of the current pixel (in the model image)
+          // with regard to the top-left pixel of the current "set of pixels"
+          int delta_i = 3 * (dy * (width) + dx);
 
+          // Accumulate RGB values
+          red   += (image)[i0+delta_i];
+          green += (image)[i0+delta_i+1];
+          blue  += (image)[i0+delta_i+2];
+          
+        }
+      }
 
+      // Divide RGB values to get the mean values
+      red   /= factor_squared;
+      green /= factor_squared;
+      blue  /= factor_squared;
 
+      // Set new pixel's RGB values
+      new_image[ 3 * (y * new_width + x) ]     = red;
+      new_image[ 3 * (y * new_width + x) + 1 ] = green;
+      new_image[ 3 * (y * new_width + x) + 2 ] = blue;
+    }
+  }
 
+  // Update image size
+  width  = new_width;
+  height = new_height;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  // Update image
+  delete image ;
+  image = new_image;
+}
