a11bddbd2a2b90fb18ca7d29f2b0334824a53a2c
Jaure Nathalie <njaure@bs406-s16-10.insa-lyon.fr>
2014-11-27 18:22:36 +0100

    Addition of ifndef

--- ppm.h
+++ ppm.h
@@ -1,31 +1,72 @@
 
+#ifndef __PICTURE_H__
+#define __PICTURE_H__
+
+// ===========================================================================
+//                                  Libraries
+// ===========================================================================
+
 
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+//============================================================================
+//                           Class definition
+//============================================================================
 
 
-//============================================================================
-//                           Function declarations
-//============================================================================
+class picture
+{
+
+private:
+
+
+  int width;
+  int height;
+  u_char *image;
+  int x;
+  int y;	
+
+public:
+
+// Definition of Getters
+int getwidth() const;
+int getheight() const;
+u_char* getimage() const;
+
+// Creation of Default constructor
+picture();
+
+// Creation of Copy constructor
+picture(const picture& model);
+
+
+// Definition of Destructor
+~picture(void);
+
+// =======================================================================
+//                              Public Methods declaration
+// =======================================================================
+
 // Write the image contained in <data> (of size <width> * <height>)
 // into plain RGB ppm file <file>
-
-
-
-
-void ppm_write_to_file(int width, int height, u_char* data, FILE* file, char* img);
+void ppm_write_to_file(FILE* file,const char* img);
 
 // Read the image contained in plain RGB ppm file <file>
 // into <data> and set <width> and <height> accordingly
-// Warning: data is malloc_ed, don't forget to free it
-void ppm_read_from_file(int *width, int *height, u_char** data, FILE* file,char* img );
+void ppm_read_from_file(FILE* file, const char* img );
 
 // Desaturate (transform to B&W) <image> (of size <width> * <height>)
-void ppm_desaturate(u_char* image,int width, int height, int x, int y );
+void ppm_desaturate();
 
 // Shrink image (of original size <width> * <height>) by factor <factor>
 // <width> and <height> are updated accordingly
-void ppm_shrink(u_char** image, int *width, int *height, int factor, int x, int y );
+void ppm_shrink( int factor);
+
+};
+
+
+#endif // __PICTURE_H__
+
